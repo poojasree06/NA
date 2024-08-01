41cd5b94a718f28111828a5ab7e05cde30fa3223
poojasree06 <poojasree.ambati@gmail.com>
2024-08-01 17:35:29 +0530

    com

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
+console.log("backend")
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
